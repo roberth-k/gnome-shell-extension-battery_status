4acbac4cb25f4ad3c8f76d238b8dd0d46c0c71b1
milliburn <roberth@winter-weht.net>
2014-05-02 16:54:05 +0100

    include licence

--- battery_status@milliburn.github.com/prefs.js
+++ battery_status@milliburn.github.com/prefs.js
@@ -1,3 +1,28 @@
+/*
+ * Gnome Shell Extension: battery_status
+ * 
+ * (2014) written and maintained by
+ *    Roberth Kulbin <roberth@winter-weht.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Alternatively, you can redistribute and/or modify this program under the
+ * same terms that the “gnome-shell” or “gnome-shell-extensions” software
+ * packages are being distributed by The GNOME Project.
+ *
+ */
+ 
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Convenience = Me.imports.convenience;
 const Settings = 
