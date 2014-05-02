e28cf2e44e2b590c283f4442ad7c9f1546cd6cbf
milliburn <roberth@winter-weht.net>
2014-05-02 02:13:41 +0100

    implement core features

--- battery_status@milliburn/extension.js
+++ battery_status@milliburn/extension.js
@@ -1,53 +1,111 @@
 
 const St = imports.gi.St;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
+const UPower = imports.ui.status.power.UPower;
+const PowerIndicator = Main.panel.statusArea.aggregateMenu._power;
 
-let text, button;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
+let debug  = true;
+let label  = null;
+let handle = null;
 
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
-    }
-
-    text.opacity = 255;
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(Math.floor(monitor.width / 2 - text.width / 2),
-                      Math.floor(monitor.height / 2 - text.height / 2));
-
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
-}
+let cfg_displayMode  = "time";
+let cfg_timeMode     = "canonical";
+let cfg_showWhenFull = true;
 
 function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({ icon_name: 'system-run-symbolic',
-                             style_class: 'system-status-icon' });
-
-    button.set_child(icon);
-    button.connect('button-press-event', _showHello);
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+  if (! PowerIndicator._proxy.Type == UPower.DeviceKind.Battery) {
+    return;
+  }
+
+  label = new St.Label();
+  update();
+  
+  PowerIndicator.indicators.add(
+    label, { y_align: St.Align.MIDDLE, y_fill: false });
+    
+  handle = PowerIndicator._proxy.connect('g-properties-changed', update);
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+  if (label) {
+    PowerIndicator.indicators.remove_child(label);
+    label.destroy();
+    label = null;
+  }
+  
+  if (handle) {
+    PowerIndicator._proxy.disconnect(handle);
+    handle = null;
+  }
 }
+
+function format_time(time_s) {
+ let mins    = Math.round(time_s / 60);
+  
+ let hrs     = Math.floor(mins / 60);
+ let hr_mins = mins % 60;
+  
+ switch (cfg_timeMode) {
+ default:
+ case "canonical":
+   return "%d:%02d".format(hrs, hr_mins);
+ }
+}
+
+function format_percent(per_c) {
+  return "%d%%".format(Math.floor(per_c));
+}
+
+function format_label(per_c, time_s) {
+  switch (cfg_displayMode) {
+    case "time":
+      return format_time(time_s);
+    case "percentage":
+    default:
+      return format_percent(per_c);
+  }
+}
+
+function update() {
+  let tte_s = PowerIndicator._proxy.TimeToEmpty;
+  let ttf_s = PowerIndicator._proxy.TimeToFull;
+  let per_c = PowerIndicator._proxy.Percentage;
+  
+  let text  = "";
+  
+  if (PowerIndicator._proxy.IsPresent) {
+    switch (PowerIndicator._proxy.State) {
+      case UPower.DeviceState.CHARGING:
+        if (ttf_s > 0) {
+          // sometimes batteries put actual capacity below 100%
+          text = format_label(per_c, ttf_s);
+          break;
+        }
+        // *fallthrough*
+        // if ttf is nought, be informative if so required
+      case UPower.DeviceState.FULLY_CHARGED:
+        if (cfg_showWhenFull) {
+          text = format_percent(per_c);
+        }
+        break;
+      case UPower.DeviceState.EMPTY:
+        text = "--";
+        break;
+      case UPower.DeviceState.UNKNOWN:
+        text = "??";
+        break;
+      case UPower.DeviceState.DISCHARGING:
+        text = format_label(per_c, tte_s);
+        break;
+    }
+  }
+  
+  label.set_text(text);
+}
+
