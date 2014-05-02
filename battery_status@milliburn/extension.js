0aef18607cce3941ec95688e0031448296e9436a
milliburn <roberth@winter-weht.net>
2014-05-02 14:06:02 +0100

    implement preferences, different time styles

--- battery_status@milliburn/extension.js
+++ battery_status@milliburn/extension.js
@@ -1,6 +1,8 @@
 
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Convenience = Me.imports.convenience;
+const Settings = 
+  Convenience.getSettings("org.gnome.shell.extensions.battery_status");
 
 const St = imports.gi.St;
 const Main = imports.ui.main;
@@ -8,36 +10,26 @@ const UPower = imports.ui.status.power.UPower;
 const PowerIndicator = Main.panel.statusArea.aggregateMenu._power;
 
 let label  = null;
-let handle = null;
+let signals = [];
+let cfg = {};
 
-let cfg_displayMode       = "time";
-let cfg_timeMode          = "canonical";
-let cfg_showWhenFull      = true;
-let cfg_showLabelWhenFull = true;
+let update_callback = null;
 
 function init() {
 }
 
 function enable() {
-  if (PowerIndicator._proxy.Type != UPower.DeviceKind.Battery) {
-    return;
-  }
+  init_settings();
   
-  let settings = 
-    Convenience.getSettings("org.gnome.shell.extensions.battery_status");
-  
-  cfg_displayMode       = settings.get_enum   ("display-mode");
-  cfg_timeMode          = settings.get_enum   ("time-mode");
-  cfg_showWhenFull      = settings.get_boolean("show-when-full");
-  cfg_showLabelWhenFull = settings.get_boolean("show-label-when-full");
-
   label = new St.Label();
-  update();
   
-  PowerIndicator.indicators.add(
-    label, { y_align: St.Align.MIDDLE, y_fill: false });
+  update_show();
     
-  handle = PowerIndicator._proxy.connect('g-properties-changed', update);
+  signals.push([PowerIndicator._proxy,
+                PowerIndicator._proxy.connect('g-properties-changed',
+                                              function () {
+                                                update_callback.call(this);
+                                              })]);
 }
 
 function disable() {
@@ -47,22 +39,68 @@ function disable() {
     label = null;
   }
   
-  if (handle) {
-    PowerIndicator._proxy.disconnect(handle);
-    handle = null;
+  while (signals.length > 0) {
+    let [obj, sig] = signals.pop();
+    obj.disconnect(sig);
   }
+  
+  update_callback = null;
+}
+
+function restart() {
+  disable();
+  enable();
+}
+
+function init_settings() {
+  function set(key, def) {
+    signals.push([Settings, Settings.connect("changed::" + key, restart)]);
+
+    switch(typeof(def)) {
+      case "boolean":
+        return Settings.get_boolean(key);
+      default:
+        return Settings.get_string(key);
+    }
+  }
+
+  cfg = {
+    displayMode : set('display-mode', 'time'),
+    timeMode    : set('time-mode'   , 'canonical'),
+    whenFull    : set('when-full'   , 'all'),
+    timeStyle   : set('time-style'  , 'ticker'),
+  };
 }
 
 function format_time(time_s) {
  let mins    = Math.round(time_s / 60);
-  
+ 
  let hrs     = Math.floor(mins / 60);
  let hr_mins = mins % 60;
+ 
+ let hr_chr  = ':';
+ let min_chr = '';
+ 
+ switch (cfg.timeStyle) {
+  default:
+  case 'ticker':
+    break;
+  case 'names':
+    hr_chr  = "h";
+    min_chr = "m";
+    break;
+  case 'geo':
+    hr_chr  = "'";
+    min_chr = '"';
+    break;
+ }
   
- switch (cfg_timeMode) {
+ switch (cfg.timeMode) {
  default:
  case "canonical":
-   return "%d:%02d".format(hrs, hr_mins);
+   return "%d%s%02d%s".format(hrs, hr_chr, hr_mins, min_chr);
+ case "flat_minutes":
+   return "%d%s".format(mins, min_chr); 
  }
 }
 
@@ -71,13 +109,21 @@ function format_percent(per_c) {
 }
 
 function format_label(per_c, time_s) {
-  switch (cfg_displayMode) {
+  switch (cfg.displayMode) {
     case "time":
       return format_time(time_s);
     case "percentage":
     default:
       return format_percent(per_c);
   }
+}
+
+function update_show() {
+  update_callback = update;
+  PowerIndicator.indicators.add(
+    label, { y_align: St.Align.MIDDLE, y_fill: false });
+  PowerIndicator.indicators.show();
+  update();
 }
 
 function update() {
@@ -95,15 +141,24 @@ function update() {
         // *fallthrough*
       case UPower.DeviceState.CHARGING:
         if (ttf_s > 0) {
-          // sometimes batteries report actual max capacity as below 100%
+          // sometimes batteries' max capacity is below 100%
+          // (e.g. when charge thresholds are in effect)
           text += format_label(per_c, ttf_s);
           break;
         }
         // *fallthrough* in case the battery driver is confused
         // whether it's still charging or not.
       case UPower.DeviceState.FULLY_CHARGED:
-        if (cfg_showWhenFull) {
-          text = format_percent(per_c);
+        switch (cfg.whenFull) {
+          case 'nothing':
+            PowerIndicator.indicators.hide();
+            update_callback = update_show;
+            break;
+          case 'all':
+            text = format_percent(per_c);
+            // *fallthrough*
+          case 'icon':
+            break;
         }
         break;
       case UPower.DeviceState.EMPTY:
---
+++ battery_status@milliburn/prefs.js
@@ -0,0 +1,85 @@
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Convenience = Me.imports.convenience;
+const Settings = 
+  Convenience.getSettings("org.gnome.shell.extensions.battery_status");
+
+const Gtk = imports.gi.Gtk;
+
+function init() {
+}
+
+function buildPrefsWidget() {
+  let frame = new Gtk.Box({
+    orientation: Gtk.Orientation.VERTICAL,
+    border_width: 10,
+    expand: true,
+  });
+  
+  let vbox = new Gtk.Box({
+    orientation: Gtk.Orientation.VERTICAL,
+    margin: 20,
+    margin_top: 10,
+  });
+  
+  let options = [
+    [ "Display mode",
+      make_combo('display-mode', 
+                 ['Time', 'Percentage']) ],
+    [ "Format time as",
+      make_combo('time-mode', 
+                 ['Canonical (h:mm)',
+                  'Minutes only (mm)']) ],
+    [ "Style time as",
+      make_combo('time-style', 
+                 ['Canonical (1:30)',
+                  'Labels (1h30m)',
+                  'Angular (1\'30")']) ],
+    [ 'When battery full, show',
+      make_combo('when-full', ['Nothing',
+                               'Icon only',
+                               'Icon and label']) ],
+  ];
+  
+  for (var i in options) {
+    vbox.add(make_option(options[i][0], options[i][1].call(this)));
+  }
+  
+  frame.add(vbox);
+  frame.show_all();
+  return frame;
+}
+
+function make_combo(pref, values) {
+  return function() {
+    let combo = new Gtk.ComboBoxText();
+  
+    for (var i in values) {
+      combo.append_text(values[i]);
+    }
+
+    combo.set_active(Settings.get_enum(pref));
+  
+    combo.connect("changed", function () {
+      Settings.set_enum(pref, combo.get_active());
+    });
+  
+    return combo;
+  }
+}
+
+function make_option(text, item) {
+  let hbox = new Gtk.Box({
+    orientation: Gtk.Orientation.HORIZONTAL,
+  });
+
+  let label = new Gtk.Label({
+    label: text,
+    xalign: 0,
+  });
+  
+  hbox.pack_start(label, true, true, 0);
+  hbox.add(item);
+  
+  return hbox;
+}
+
